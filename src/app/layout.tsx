f4813a16618eb8548bb9b227a295071c1742bb91
Pedro Henrique <pedro795027@gmail.com>
2024-11-17 16:11:54 -0300

    feat: implementação de autenticação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextAuthSessionProvider from "@/providers/sessionProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.variable}>{children}</body>
+      <body className={inter.variable}>
+        <NextAuthSessionProvider>{children}</NextAuthSessionProvider>
+      </body>
     </html>
   );
 }
